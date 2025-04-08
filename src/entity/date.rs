f0adb467e7f79fee54401046a90d0cb154013e67
Sumit Datta <sumitdatta@gmail.com>
2025-04-08 08:12:45 +0530

    WIP tests and mock for entity extraction process; Date data type only for now

--- src/entity/date.rs
+++ src/entity/date.rs
@@ -1,13 +1,24 @@
-pub struct Date;
+use super::{EntityExtractionRequest, EntityLabel, ExtractedData, ExtractedEntity};
+use crate::error::DetResult;
+pub struct DetDate;
 
 // TODO: Implement these functions as a common Trait
-impl Date {
+impl DetDate {
     pub fn extraction_data_type() -> String {
         "ISO 8601 Date".to_string()
     }
 
-    pub fn process() -> String {
+    pub fn process(
+        _text: &str,
+        extraction_request: &EntityExtractionRequest,
+        extracted_data: &ExtractedData,
+    ) -> DetResult<ExtractedEntity> {
         // This would take the `ExtractedEntity` and return a Rust native `Date` object
-        "".to_string()
+        Ok(ExtractedEntity {
+            entity_label: EntityLabel::Date,
+            matched_text: "Jan 23, 2022".to_string(),
+            variable_name: extraction_request.variable_name.clone(),
+            start: 34,
+        })
     }
 }
